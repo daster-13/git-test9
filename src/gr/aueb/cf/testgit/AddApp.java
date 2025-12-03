346857738da84559577b7332d9022ddef74eb4e5
Daster <dionusia.asteropoulou@gmail.com>
2025-12-03 10:57:01 +0200

    feat:add  subapp

--- src/gr/aueb/cf/testgit/AddApp.java
+++ src/gr/aueb/cf/testgit/AddApp.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.testgit;
 
 public class AddApp {
     static void main() {
+        System.out.println(add(1, 2 ));
 
     }
     public static int add(int a, int b){
---
+++ src/gr/aueb/cf/testgit/SubApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.testgit;
+
+public class SubApp {
+    static void main() {
+        System.out.println(SubApp(10, 5));
+
+    }
+    public static  int SubApp( int a, int b) {
+        return a - b;
+    }
+}
